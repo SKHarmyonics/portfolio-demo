49e7d7948ce547e0306eb15901f49cb0b8b9e557
Siddhant Kumar <skharmyonics@outlook.com>
2024-06-09 01:24:06 +0530

    Final Website

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 
 const page = () => {
   return (
-    <div className="min-h-screen bg-black overflow-hidden pl-20">
+    <div className="min-h-screen bg-black overflow-hidden">
       <div className=" dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
         <div className="max-w-7xl mx-auto p-5">
           <Navbar className="flex" />
